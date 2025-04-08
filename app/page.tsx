bb34c14d02ac8c6738d4a73e0cf3c0e50434902f
Robin <21104964@student.hhs.nl>
2025-04-08 16:54:19 +0200

    Inputvelden toegevoegd

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+import { useState } from "react";
+import "./globals.css";
+
 export default function Home() {
-    return <>JuiceDarts</>;
+    const [naam, setNaam] = useState("");
+
+    const opties = ["Robin", "Siebe", "Jacky", "Bart"];
+
+    return (
+        <>
+            {/* <div className="Titel">Kies een naam:</div> */}
+            <div className="inputvelden">
+                <select
+                    name="naam"
+                    value={naam}
+                    onChange={(e) => setNaam(e.target.value)}
+                    className="dropdown"
+                >
+                    <option value="">Naam</option>
+                    {opties.map((optie) => (
+                        <option key={optie} value={optie}>
+                            {optie}
+                        </option>
+                    ))}
+                </select>
+                <input
+                    name="eerste20"
+                    className="input"
+                    placeholder="Eerste 20"
+                ></input>
+                <input
+                    name="totaal"
+                    className="input"
+                    placeholder="Totaal"
+                ></input>
+            </div>
+        </>
+    );
 }
