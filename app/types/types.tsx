106789e0a8e93929df005af2c30bb915ebc144d8
Robin <21104964@student.hhs.nl>
2025-04-13 21:29:19 +0200

    types toegevoegd

---
+++ app/types/types.tsx
@@ -0,0 +1,11 @@
+export type User = {
+    id: number;
+    userName: string;
+};
+export type rondjeScore = {
+    Id: number;
+    userName: string;
+    eersteTwintig: number;
+    totaal: number;
+    date: string;
+};
